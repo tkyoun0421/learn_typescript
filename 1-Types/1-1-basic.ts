438d3ee5c1c4136b71530e24e5cf0d00eb9d9527
태관 <tkyoun0421@naver.com>
2023-12-18 17:00:15 +0900

    update basic.ts

--- 1-Types/1-1-basic.ts
+++ 1-Types/1-1-basic.ts
@@ -1,14 +1,27 @@
 {
-  // Javascript
-  // old: var [Bad!!]
-  var age = 5;
+  /**
+   * Javascript
+   * Primitive: number, string, boolean, bigint, symbol, undefined
+   * Object: function, ary......
+   */
+
+  // number
+  const num: number = "d"; // type error!
+  const number: number = 1; // ok!
+
+  // string
+  const str: string = "hello";
+
+  // boolean
+  const boal: boolean = false;
+
+  // undefined
+  let name: undefined; // Bad!!
+  let age: number | undefined;
+  age = undefined;
   age = 1;
 
-  // let es6
-  let name = "hello";
-  name = "hi";
-
-  // const
-  const count = 5;
-  count = 3;
+  // null
+  let person: null; // Bad!!
+  let person2: string | null;
 }
