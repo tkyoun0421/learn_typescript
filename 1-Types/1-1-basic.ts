a07706776327f3e4433cbfa5ac124590dbb87d4d
태관 <tkyoun0421@naver.com>
2023-12-18 17:05:56 +0900

    update basic.ts 기본 타입 정리

--- 1-Types/1-1-basic.ts
+++ 1-Types/1-1-basic.ts
@@ -24,4 +24,32 @@
   // null
   let person: null; // Bad!!
   let person2: string | null;
+
+  // unknown
+  let notSure: unknown = 0;
+  notSure = "he";
+  notSure = true;
+
+  // any
+  let anything: any = 0;
+  anything = "hello";
+
+  // void
+  function print(): void {
+    console.log("hello");
+    return;
+  }
+  let unusable: void = undefined; // Bad!
+
+  // never
+  function throwError(message: string): never {
+    // message -> server (log)
+    throw new Error(message);
+  }
+
+  // object
+  let obj: object; // Bad!
+  function acceptSomeObject(obj: object) {}
+  acceptSomeObject({ name: "ellie" });
+  acceptSomeObject({ animal: "dog" });
 }
