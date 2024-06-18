48dbfc84e9d11e686a93ce682e0b80d3a5ffbc83
태관 <tkyoun0421@naver.com>
2024-06-18 14:11:38 +0900

    페이지 아이템 컴포넌트

--- 10-Project/dist/components/page/page.js
+++ 10-Project/dist/components/page/page.js
@@ -1,6 +1,25 @@
 import { BaseComponent } from "../component.js";
 export class PageComponent extends BaseComponent {
     constructor() {
-        super('<ul class="page">This is PageComponent</ul>');
+        super('<ul class="page"></ul>');
+    }
+    addChild(section) {
+        const item = new PageItemComponent();
+        item.addChild(section);
+        item.attachTo(this.element, "beforeend");
+    }
+}
+export class PageItemComponent extends BaseComponent {
+    constructor() {
+        super(`<li class="page-item">
+            <section class="page-item__body"></section>
+            <div class="page-item__controls">
+              <span class="close">&times;</span>
+            </div>
+          </li>`);
+    }
+    addChild(child) {
+        const container = this.element.querySelector(".page-item__body");
+        child.attachTo(container);
     }
 }
